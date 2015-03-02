13c777536ecfcec678c80637baca5214ee0c67b9
Janis Fliegenschmidt <janisf@gmx.net>
2015-03-02 17:30:20 +0100

    We do now terminate on request (actually, solely on request.) Also, logging to understand how deep the tree traversal went. As of this commit, the plugin is rule-compliant, unless exceptions are obligatory (but I just can't see why they would be.)

--- src/nl/tue/s2id90/group41/AlphaBetaPlayer.java
+++ src/nl/tue/s2id90/group41/AlphaBetaPlayer.java
@@ -5,16 +5,21 @@
  */
 package nl.tue.s2id90.group41;
 
+import java.util.logging.Level;
 import nl.tue.s2id90.draughts.DraughtsState;
 import nl.tue.s2id90.draughts.player.DraughtsPlayer;
 import org10x10.dam.game.Move;
+import java.util.logging.Logger;
 
 /**
  * @author Janis Fliegenschmidt
  */
 public abstract class AlphaBetaPlayer extends DraughtsPlayer {
 
+    private static final Logger LOG = Logger.getLogger(AlphaBetaPlayer.class.getName());
+
     private int value;
+    private boolean stopped;
 
     public AlphaBetaPlayer() {
         super(UninformedPlayer.class.getResource("resources/optimist.png"));
@@ -22,6 +27,7 @@ public abstract class AlphaBetaPlayer extends DraughtsPlayer {
 
     @Override
     public Move getMove(DraughtsState draughtsState) {
+        this.stopped = false;
         return AlphaBeta(draughtsState);
     }
 
@@ -30,18 +36,28 @@ public abstract class AlphaBetaPlayer extends DraughtsPlayer {
         return this.value;
     }
 
-    private Move AlphaBeta(DraughtsState state) {
-        GameNode node = new GameNode(state);
-
-        int depth = 0;
-        do {
-            this.value = AlphaBeta(node, Integer.MIN_VALUE, Integer.MAX_VALUE, depth++);
-        } while (depth < alphaBetaDepth);
-
-        return node.getBestMove();
+    @Override
+    public void stop() {
+        this.stopped = true;
     }
 
-    private final int alphaBetaDepth = 10;
+    private Move AlphaBeta(DraughtsState state) {
+        GameNode node = new GameNode(state);
+        node.setBestMove(node.getState().getMoves().get(0));
+
+        Move move = null;
+        int tempValue = Integer.MIN_VALUE;
+        int depth = 0;
+        do {
+            move = node.getBestMove();
+            this.value = tempValue;
+            tempValue = AlphaBeta(node, Integer.MIN_VALUE, Integer.MAX_VALUE, depth++);
+        } while (!this.IsStopped());
+
+        LOG.log(Level.INFO, "Search depth achieved: {0}", --depth);
+
+        return move;
+    }
 
     private int AlphaBeta(GameNode node, int alpha, int beta, int depth) {
         if (node.getState().isWhiteToMove()) {
@@ -76,7 +92,7 @@ public abstract class AlphaBetaPlayer extends DraughtsPlayer {
     }
 
     private int AlphaBetaMax(GameNode node, int alpha, int beta, int depth) {
-        if (depth <= 0 || node.getState().isEndState()) {
+        if (depth <= 0 || node.getState().isEndState() || this.IsStopped()) {
             return GetRating(node);
         }
 
@@ -99,7 +115,7 @@ public abstract class AlphaBetaPlayer extends DraughtsPlayer {
     }
 
     private int AlphaBetaMin(GameNode node, int alpha, int beta, int depth) {
-        if (depth <= 0 || node.getState().isEndState()) {
+        if (depth <= 0 || node.getState().isEndState() || this.IsStopped()) {
             return GetRating(node);
         }
 
@@ -122,4 +138,8 @@ public abstract class AlphaBetaPlayer extends DraughtsPlayer {
     }
 
     public abstract int GetRating(GameNode node);
+
+    protected boolean IsStopped() {
+        return this.stopped;
+    }
 }
