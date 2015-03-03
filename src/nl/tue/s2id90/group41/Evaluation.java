7cc83950afe3e714c60cf07a62da84d2f18461a1
Alex Au <aualex7@gmail.com>
2015-03-03 23:45:45 +0100

    Recreated the auxiliary method for enMasse, where I abstracted out into Black and White enMasse due to different directions. Also noticed that different nodes had different TR, TL, BL, BR patterns and had to account for them as well

--- src/nl/tue/s2id90/group41/Evaluation.java
+++ src/nl/tue/s2id90/group41/Evaluation.java
@@ -5,6 +5,8 @@
  */
 package nl.tue.s2id90.group41;
 
+import java.util.Arrays;
+import java.util.List;
 import nl.tue.s2id90.draughts.DraughtsState;
 /**
  *
@@ -101,32 +103,136 @@ public class Evaluation extends AlphaBetaPlayer {
         int rating = 0;
         int[] piece = state.getPieces();
         
-        //for-loop in order to target middle nodes in the game boards
+        //pattern for the TL, TR, BR, BL for 7,8,9,10 etc
+        List<Integer> pattern1 = Arrays.asList(-6,-5,5,4);
+        
+        //pattern for the TL, TR, BR, BL for 7,8,9,10 etc
+        List<Integer> pattern2 = Arrays.asList(-5,-4,6,5);
+        
+        //excludes border nodes, focus on center nodes
         for (int i = 0; i < 31; i = i + 10) {
             
-            //for-loop for 8 nodes within two rows
-            for (int j = i + 7; j < i + 15; j++ ) {
-                if (piece[j] == DraughtsState.BLACKPIECE) {
-                    //the pieces that can block capture are on nodes -6, -5
-                    if (piece[j-6] == DraughtsState.BLACKPIECE) {
-                        ++rating;
-                    }
-                    if (piece[j-5] == DraughtsState.BLACKPIECE) {
-                        ++rating;
-                    }
-                
-                } else if (piece[j] == DraughtsState.WHITEPIECE) {
-                    //the pieces that can block capture are on nodes +6, +5
-                    if (piece[j+6] == DraughtsState.WHITEPIECE) {
-                        --rating;
-                    }
-                    if (piece[j+5] == DraughtsState.WHITEPIECE) {
-                        --rating;
-                    }
+            //first four nodes
+            for (int j = i + 7; j < i + 11; j++) {
+                int foo = piece[j];
+                if (evalBlackPiece(foo)) {
+                    rating += enMasseBlack(pattern1, foo, piece);
+                } else if (evalWhitePiece(foo)) {
+                    rating -= enMasseWhite(pattern1, foo, piece);
                 }
-            }   
+            }
+            
+            //second four nodes
+            for (int k = i + 11; k < i + 15; k++) {
+                int bar = piece[k];
+                if (evalBlackPiece(bar)) {
+                    rating += enMasseBlack(pattern2, bar, piece);
+                } else if (evalWhitePiece(bar)) {
+                    rating -= enMasseWhite(pattern2, bar, piece);
+                }
+            }      
         }
         
         return rating;
     }
+    
+    private int enMasseBlack(List<Integer> pattern, int abc, int[] piece) {
+        int rating = 0;
+        
+        //determines the top left, top right, bottom right, and bottom left
+        int TL = piece[abc + pattern.get(0)];
+        int TR = piece[abc + pattern.get(1)];
+        int BR = piece[abc + pattern.get(2)];
+        int BL = piece[abc + pattern.get(3)];
+        
+        //determines if top left has enemy king and whether it can be blocked
+        if (evalWhiteKing(TL)) {
+            if (evalBlackPiece(BR)) {
+                rating += 2;
+            }    
+        }
+        
+        //determines if top right has enemy king and whether it can be blocked
+        if (evalWhiteKing(TR)) {
+            if (evalBlackPiece(BL)) {
+                rating += 2;
+            }
+        }
+        
+        //determines if top left has black piece or king and whether it has blocked a capture
+        if (evalBlackPiece(TL)) {
+            if (evalWhitePiece(BR)) {
+                rating += 2;
+            } else {
+                ++rating;
+            }
+        }
+        
+        //determines if top right has black piece or king and whether it has blocked a capture
+        if (evalBlackPiece(TR)) {
+            if (evalWhitePiece(BL)) {
+                rating += 2;
+            } else {
+                ++rating;
+            }
+        }
+        
+        return rating;
+    }
+    
+    private int enMasseWhite(List<Integer> pattern, int abc, int[] piece) {
+        int rating = 0;
+        
+        int TL = piece[abc + pattern.get(0)];
+        int TR = piece[abc + pattern.get(1)];
+        int BR = piece[abc + pattern.get(2)];
+        int BL = piece[abc + pattern.get(3)];
+        
+        if (evalBlackKing(BL)) {
+            if (evalWhitePiece(TR)) {
+                rating += 2;
+            }    
+        }
+        
+        if (evalBlackKing(BR)) {
+            if (evalWhitePiece(TL)) {
+                rating += 2;
+            }
+        }
+        
+        if (evalWhitePiece(BL)) {
+            if (evalBlackPiece(TR)) {
+                rating += 2;
+            } else {
+                ++rating;
+            }
+        }
+        
+        if (evalWhitePiece(BR)) {
+            if (evalBlackPiece(TL)) {
+                rating += 2;
+            } else {
+                ++rating;
+            }
+        }
+        
+        return rating;
+    }
+    
+    private boolean evalBlackPiece(int x) {
+        return (x == DraughtsState.BLACKPIECE || x == DraughtsState.BLACKKING);
+    }
+    
+    private boolean evalBlackKing(int x) {
+        return (x == DraughtsState.BLACKKING);
+    }
+    
+    private boolean evalWhitePiece(int x) {
+        return (x == DraughtsState.WHITEPIECE || x == DraughtsState.WHITEKING);
+    }
+    
+    private boolean evalWhiteKing(int x) {
+        return (x == DraughtsState.WHITEKING);
+    }
+    
 }
