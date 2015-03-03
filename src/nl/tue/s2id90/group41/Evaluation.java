b04e5b296c6153e05c7df9442faa1541e94c6bb6
Alex Au <aualex7@gmail.com>
2015-03-03 14:58:40 +0100

    Created auxiliary methods for midDomination and enMasse

--- src/nl/tue/s2id90/group41/Evaluation.java
+++ src/nl/tue/s2id90/group41/Evaluation.java
@@ -15,13 +15,17 @@ public class Evaluation extends AlphaBetaPlayer {
     @Override
     public int GetRating(GameNode node) {
        DraughtsState state = node.getState();
-       int rating = piececount(state);
+       int rating = 0;
+       
+       rating += pieceCount(state);
        rating += kingDefense(state);
+       rating += midDomination(state);
+       rating += enMasse(state) * 0.5;
        
        return rating;
     }
     
-    private int piececount(DraughtsState state) {
+    private int pieceCount(DraughtsState state) {
         int rating = 0;
         for (int piece : state.getPieces()) {
             switch (piece) {
@@ -61,5 +65,63 @@ public class Evaluation extends AlphaBetaPlayer {
         return rating;
     }
     
+    private int midDomination(DraughtsState state) {
+        int rating = 0;
+        int[] piece = state.getPieces();
+        
+        for (int i = 21; i < 31; i++) {
+            //Janis, does this make sense?
+            switch (piece[i]) {
+                    case DraughtsState.BLACKKING:
+                    case DraughtsState.BLACKPIECE:
+                        ++rating;
+                        break;
+
+                    case DraughtsState.WHITEKING:
+                    case DraughtsState.WHITEPIECE:
+                        --rating;
+                        break;
+                }
+            }
+        
+        return rating;
+    }
     
+    /**
+     * Returns a rating based on advancing together, in order to prevent attacks
+     * @param state
+     * @return integer
+     */
+    private int enMasse(DraughtsState state) {
+        int rating = 0;
+        int[] piece = state.getPieces();
+        
+        //for-loop in order to target middle nodes in the game boards
+        for (int i = 0; i < 31; i = i + 10) {
+            
+            //for-loop for 8 nodes within two rows
+            for (int j = i + 7; j < i + 15; j++ ) {
+                if (piece[j] == DraughtsState.BLACKPIECE) {
+                    //the pieces that can block capture are on nodes -6, -5
+                    if (piece[j-6] == DraughtsState.BLACKPIECE) {
+                        ++rating;
+                    }
+                    if (piece[j-5] == DraughtsState.BLACKPIECE) {
+                        ++rating;
+                    }
+                
+                } else if (piece[j] == DraughtsState.WHITEPIECE) {
+                    //the pieces that can block capture are on nodes +6, +5
+                    if (piece[j+6] == DraughtsState.WHITEPIECE) {
+                        --rating;
+                    }
+                    if (piece[j+5] == DraughtsState.WHITEPIECE) {
+                        --rating;
+                    }
+                }
+            }   
+        }
+        
+        return rating;
+    }
 }
